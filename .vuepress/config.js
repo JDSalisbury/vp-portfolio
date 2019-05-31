7ef0430aa158e2fcdd23cc915a0b8f15ad800822
JDSalisbury <Salisbury.jeffery@gmail.com>
2019-05-30 23:04:59 -0400

    Setting up vuepress static site.

---
+++ .vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: "Portfolio",
+  description: "Just playing around",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Resume", link: "/resume" },
+      { text: "Projects", link: "/Projects" }
+    ]
+  }
+};
