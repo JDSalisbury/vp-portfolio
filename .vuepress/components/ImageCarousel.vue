e547e8f557423560dd8f8039c8dc29d0ee82510b
JDSalisbury <Salisbury.jeffery@gmail.com>
2019-06-04 09:58:27 -0400

    Issue with carousel i think?

--- .vuepress/components/ImageCarousel.vue
+++ .vuepress/components/ImageCarousel.vue
@@ -1,9 +1,9 @@
 <template>
   <div style="width: 60%; display:block; margin:0 auto;">
-    <carousel :per-page="1" :center-mode="true" adjustableHeight="true">
-      <slide>
-        <img src="/images/pp1.png">
-      </slide>
+    <!-- <carousel :per-page="1" :center-mode="true" adjustableHeight="true"> -->
+    <!-- <slide> -->
+    <img src="/images/pp1.png">
+    <!-- </slide>
       <slide>
         <img src="/images/pp2.png">
       </slide>
@@ -12,18 +12,18 @@
       </slide>
       <slide>
         <img src="/images/pp4.png">
-      </slide>
-    </carousel>
+    </slide>-->
+    <!-- </carousel> -->
   </div>
 </template>
 
 <script>
-import { Carousel, Slide } from "vue-carousel";
+// import { Carousel, Slide } from "vue-carousel";
 
 export default {
   components: {
-    Carousel,
-    Slide
+    // Carousel,
+    // Slide
   }
   /*
   mounted () {
