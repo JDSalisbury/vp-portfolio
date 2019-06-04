6fedbad8391536ad09570887fb2d9380d93f780a
JDSalisbury <Salisbury.jeffery@gmail.com>
2019-06-04 08:50:15 -0400

    Update again

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  base: "/JDSalisbury.github.io/",
+  base: "/jdsalisbury/",
   title: "Jeff Salisbury",
   description:
     "Self-driven software developer, with excellent interpersonal skills, outstanding work ethic, and proof-based problem-solving capabilities. Recognized team player seeking to leverage knowledge to help companies become more efficient in their technological efforts.",
