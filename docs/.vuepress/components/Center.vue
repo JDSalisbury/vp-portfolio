a7e9b6bc89cfbf945a124518024089b40a632d32
JDSalisbury <Salisbury.jeffery@gmail.com>
2019-12-21 21:31:40 -0500

    Moved centering to a Component. Just more classy.

---
+++ docs/.vuepress/components/Center.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+div {
+  display: flex;
+  justify-content: center;
+}
+</style>
